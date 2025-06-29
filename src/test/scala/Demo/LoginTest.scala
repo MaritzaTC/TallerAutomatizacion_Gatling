89e7510665eeebc47725b7eaa9e33abe3bcb414a
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-06-28 19:07:35 -0500

    feat: add login data CSV and update LoginTest to use it for authentication

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -5,31 +5,45 @@ import io.gatling.http.Predef._
 import Demo.Data._
 
 class LoginTest extends Simulation{
-
+ val feeder = csv("loginData.csv").circular
   // 1 Http Conf
   val httpConf = http.baseUrl(url)
     .acceptHeader("application/json")
     //Verificar de forma general para todas las solicitudes
-    .check(status.is(200))
 
   // 2 Scenario Definition
-  val scn = scenario("Login").
-    exec(http("login")
+  val scn = scenario("Login con datos CSV")
+  .feed(feeder) 
+   .exec(http("login")
       .post(s"users/login")
-      .body(StringBody(s"""{"email": "${email}", "password": "${password}"}""")).asJson
-      //Validar status 200 del servicio
+      .body(StringBody(s"""{"email": "$email", "password": "$password"}""")).asJson
+         //Validar status 200 del servicio
       .check(status.is(200))
-      .check (jsonPath("$.token").saveAs("authToken")) // Guardar el token en una variable
-    ).exec(
-      http("Create Contact")
-      .post(s"contacts")
-      .header("Authorization", session => s"Bearer ${session("authToken").as[String]}")
-      .body(StringBody(s"""{"firstName": "${firstName}", "lastName": "${lastName}","birthdate": "${birthdate}","email": "${contactEmail }", "phone": "${phone}"  }""")).asJson
-      .check(status.is(200))
+      .check(jsonPath("$.token").saveAs("authToken"))
     )
+    .exec(
+      http("Create Contact")
+        .post(s"contacts")
+        .header("Authorization", "Bearer ${authToken}")
+        .body(StringBody(s""" {
+      "firstName": "John",
+      "lastName": "Doe",
+      "birthdate": "1970-01-01",
+      "email": "jdoe@fake.com",
+      "phone": "8005555555",
+      "street1": "1 Main St.",
+      "street2": "Apartment A",
+      "city": "Anytown",
+      "stateProvince": "KS",
+      "postalCode": "12345",
+      "country": "USA"
+    }""")).asJson
+        .check(status.is(201))
+    )
+
 
   // 3 Load Scenario
   setUp(
-    scn.inject(rampUsersPerSec(5).to(15).during(30))
+    scn.inject(rampUsers(10).during(50))
   ).protocols(httpConf);
 }
